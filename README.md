7e9a2def6dcea75041fae799541fb4d70d8d3736
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-08-22 15:00:06 +0200

    feat: second part of the ex

--- README.md
+++ README.md
@@ -15,6 +15,13 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
 
+# Fase 2 Esercizio
+
+Il computer deve generare 16 numeri casuali nello stesso range della difficoltà prescelta: le bombe. Attenzione: nella stessa cella può essere posizionata al massimo una bomba, perciò nell'array delle bombe non potranno esserci due numeri uguali.
+In seguito l'utente clicca su una cella: se il numero è presente nella lista dei numeri generati - abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina. Altrimenti la cella cliccata si colora di azzurro e l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba o quando raggiunge il numero massimo possibile di numeri consentiti (ovvero quando ha rivelato tutte le celle che non sono bombe).
+Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l'utente ha cliccato su una cella che non era una bomba.
+
 ## Svolgimento
 
 L'utente clicca su un bottone che genererà una griglia di gioco quadrata
@@ -44,3 +51,13 @@ Quindi:
 - associamo all'evento click sulla cella un codice
   - usiamo una classe per cambiare colore alla cella
   - usiamo il console.log sull'indice associato alla cella
+
+## SECONDA PARTE ESERCIZIO
+
+- Faccio un array vuoto
+- Faccio una funzione che genera numeri random
+- **FINCHE'** non avro 16 numeri aggiungo i numeri random all'array
+  - se sono già presenti nell'array li scarto
+- Al click della cella
+  - **SE** è un numero presente nell'array quindi una bomba esplode e compare un alert
+  - **ALTRIMENTI** compare lo sfondo blu e continua il gioco
--- js/main.js
+++ js/main.js
@@ -1,33 +1,69 @@
 //* PRENDIAMO IL BOTTONE TRAMITE ID
 const playButton = document.getElementById("play-button");
 
+//* PRENDIAMO LA SELECT TRAMITE ID
+const selectDifficulty = document.getElementById("difficulty");
+
 //* PRENDIAMO IL DIV CHE CONTERRA' LA GRIGLIA TRAMITE ID
 let grid = document.getElementById("grid");
 
+//* DICHIARO ARRAY VUOTO
+const bombs = [];
+while (bombs.length < 16) {
+  const randomBombs = generateRandomNumber(1, 100);
+  if (!bombs.includes(randomBombs)) {
+    bombs.push(randomBombs);
+  }
+}
+console.log(bombs);
+
 //* AL CLICK GENERA LA GRIGLIA
 playButton.addEventListener("click", function () {
+  //* NUMERO DI CELLE IN BASE ALLA DIFFICOLTA'
+  let cellsTotal = 100;
+
+  if (selectDifficulty.value == 2) {
+    cellsTotal = 81;
+  } else if (selectDifficulty.value == 3) {
+    cellsTotal = 49;
+  }
+
   grid.innerHTML = "";
-  generateGrid();
+  generateGrid(cellsTotal);
 });
 
-//* FACCIAMO UNA FUNZIONE CHE GENERA UNA GRIGLIA COMPOSTA DA 100 CELLE
-function generateGrid() {
-  for (let i = 1; i <= 100; i++) {
-    generateCell(i);
+//! FUNZIONI
+
+//* FACCIAMO UNA FUNZIONE CHE GENERA UNA GRIGLIA COMPOSTA DA CELLE
+function generateGrid(cellsTotal) {
+  for (let i = 1; i <= cellsTotal; i++) {
+    generateCell(i, cellsTotal);
   }
 }
 
 //* FACCIAMO UNA FUNZIONE CHE GENERA UNA CELLA
-function generateCell(index) {
+function generateCell(index, cellsTotal) {
   const cell = document.createElement("div");
   cell.classList.add("cell");
+  cell.classList.add("cell-" + cellsTotal);
+
   cell.innerHTML = index;
 
   grid.append(cell);
 
   //* CAMBIA SFONDO AL CLICK E STAMPA L'INDICE
   cell.addEventListener("click", function () {
-    cell.classList.add("azure");
-    console.log(index);
+    if (bombs.includes(index)) {
+      cell.classList.add("red");
+      alert("BOOOM!!!");
+    } else {
+      cell.classList.add("azure");
+    }
   });
+}
+
+//* FUNZIONE CHE GENERA NUMERI RANDOM
+function generateRandomNumber(min, max) {
+  const randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+  return randomNumber;
 }
